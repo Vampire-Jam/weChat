39750bc746ae96e636fd3fff1e163c039d8d4b40
XMG <XMG@163.com>
2016-07-22 11:40:42 +0800

    创建了Person类

---
+++ weChat/weChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  weChat
+//
+//  Created by 蒋林佐 on 16/7/22.
+//  Copyright © 2016年 蒋林佐. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ weChat/weChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  weChat
+//
+//  Created by 蒋林佐 on 16/7/22.
+//  Copyright © 2016年 蒋林佐. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
